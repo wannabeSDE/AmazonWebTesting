ac8b49f6f23c83bfa13c719ffb474d961a50f833
wannabeSDE <abhishek.das0419@gmail.com>
2022-05-28 22:00:54 +0530

    "Initial Commit"

--- src/test/java/stepDefinitions/Main.java
+++ src/test/java/stepDefinitions/Main.java
@@ -4,6 +4,8 @@ import utilities.Logs;
 
 public class Main {
     public static void main(String[] args) {
+        Logs.getInfoLogs("This is a info message");
         Logs.getErrorLogs("This is a error message");
+
     }
 }
